0a1cb7726830866320c59f0f33e508de19faedc5
Rupeshrauniyar <151705269+Rupeshrauniyar@users.noreply.github.com>
2024-01-15 13:42:48 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -44,3 +44,53 @@ audio2.play();
     button.addEventListener('click', like , play1 );
     button.addEventListener('dblclick', unlike , play2);
   });
+ 
+ // Get all video elements
+const videos = document.querySelectorAll('.video');
+
+// Options for the Intersection Observer
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.7 // Change this value based on when you want the video to start playing
+};
+
+// Callback function when video is in the viewport
+function handleIntersection(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.play();
+        } else {
+            entry.target.pause();
+        }
+    });
+}
+
+// Create Intersection Observer
+const observer = new IntersectionObserver(handleIntersection, options);
+
+// Observe each video element
+videos.forEach(video => {
+    observer.observe(video);
+});
+    const page3 = document.getElementsByClassName('page2-main')
+    const button = document.getElementById('play');
+    const video = document.getElementById('video');
+
+    // Function to play the video when the hyperlink is clicked
+    const playVideoOnClick = (event) => {
+        event.preventDefault(); // Prevent the default behavior of the hyperlink
+        video.play();
+        
+    };
+   
+    
+    // Add a click event listener to the hyperlink
+    button.addEventListener('click', playVideoOnClick);
+    button.addEventListener('click' ,function(){
+    button.style.display = 'none',
+    page3.style.display = 'grid'
+    })
+     
+   
+    
